4a9d985c6df7f6b34a4cf3bd7a6ca8a4945cf6e8
Ilya Akimov` <ilya.akimov2015@yandex.ru>
2024-09-17 22:36:19 +0400

    Initial

---
+++ src/main/java/ru/akimov/template/grpc_template/GrpcTemplateApplication.java
@@ -0,0 +1,13 @@
+package ru.akimov.template.grpc_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/akimov/template/grpc_template/GrpcTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akimov.template.grpc_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
